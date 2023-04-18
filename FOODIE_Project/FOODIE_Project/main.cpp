11db4e778e6a28e46071b28ecc77247c1a348576
isabeldailey <91341452+isabeldailey@users.noreply.github.com>
2023-04-18 12:53:15 -0700

    Update main.cpp

--- FOODIE_Project/FOODIE_Project/main.cpp
+++ FOODIE_Project/FOODIE_Project/main.cpp
@@ -5,10 +5,25 @@
 //  Created by Isabel Dailey on 4/18/23.
 //
 
-#include <iostream>
+#include "rankedListGenerator.h"
+#include "stdc++.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    int orderNumber = 1;
+    string yesNo;
+    // Part B
+    do{
+        cout << "Would you like to make a new order? (y/n) ";
+        cin >> yesNo;
+        if(yesNo == "y"){ FOODIE_BAGGER(orderNumber++); }
+    
+    
+        cout << "Part A here or separate program? " << endl;
+        
+        cout << "--------------------------------------" << endl;
+        cout << "Continue? (y/n) ";
+        cin >> yesNo;
+    } while(yesNo == "y");
+
     return 0;
 }
