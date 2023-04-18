1dffcb2743aa81f2db68f03f89220fdf57b9ec5c
rfuentesgtz <70235891+rfuentesgtz@users.noreply.github.com>
2023-04-18 11:45:46 -0700

    Added sample graph for test

--- partA.cpp
+++ partA.cpp
@@ -1,21 +1,41 @@
+// A simple representation of graph using STL
 #include <iostream>
+#include <vector>
 using namespace std;
  
+// A utility function to add an edge in an
+// undirected graph.
+void addEdge(vector<int> adj[], int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+ 
+// A utility function to print the adjacency list
+// representation of graph
+void printGraph(vector<int> adj[], int V)
+{
+    for (int v = 0; v < V; ++v) {
+        cout << "\n Adjacency list of vertex " << v
+             << "\n head ";
+        for (auto x : adj[v])
+            cout << "-> " << x;
+               cout<<endl;
+    }
+}
+ 
+// Driver code
 int main()
 {
-    // n is the number of vertices
-    // m is the number of edges
-    int n, m;
-    cin >> n >> m;
-    int adjMat[n + 1][n + 1];
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        adjMat[u][v] = 1;
-        adjMat[v][u] = 1;
-        // for a directed graph with an edge pointing from u
-        // to v,we just assign adjMat[u][v] as 1
-    }
- 
+    int V = 5;
+    vector<int> adj[V];
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+    printGraph(adj, V);
     return 0;
 }
