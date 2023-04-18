d260bce6f84f1998947fafdaa82de82a9aca52b4
isabeldailey <91341452+isabeldailey@users.noreply.github.com>
2023-04-18 11:08:58 -0700

    Add files via upload

---
+++ rankedListGenerator.h
@@ -0,0 +1,11 @@
+#ifndef RANKEDLISTGENERATOR_H // To make sure you don't declare the function more than once by including the header multiple times.
+#define RANKEDLISTGENERATOR_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void FOODIE_BAGGER(int orderNumber);
+
+#endif
