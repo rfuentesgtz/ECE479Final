6771238e011742da5e0c2854afaf642117c199bd
isabeldailey <91341452+isabeldailey@users.noreply.github.com>
2023-04-25 15:35:22 -0700

    Update and rename rankedListGenerator.h to FOODIE_BAGGER.h

---
+++ FOODIE_BAGGER.h
@@ -0,0 +1,18 @@
+//
+//  rankedListGenerator.h
+//  FOODIE_Project2
+//
+//  Created by Isabel Dailey on 4/19/23.
+//
+#ifndef FOODIEBAGGER_H // To make sure you don't declare the function more than once by including the header multiple times.
+#define FOODIEBAGGER_H
+
+#include <iostream>
+#include <fstream>
+#include "stdc++.h"
+
+using namespace std;
+
+void FOODIE_BAGGER(int orderNumber);
+
+#endif
